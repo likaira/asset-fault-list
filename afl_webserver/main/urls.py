2f02733ce2a577d7b2c1c80cf4d0507a43ab2b28
Li Kaira <likaira@verdia.com.au>
2021-07-28 13:36:40 +1000

    Setup Bootstrap template

--- afl_webserver/main/urls.py
+++ afl_webserver/main/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from . import views
+from .views import (
+    PVSystemCreateView, PVSystemDetailView, PVSystemListView, PVSystemUpdateView, PVSystemDeleteView,
+)
 
 urlpatterns = [
+    #Landing Page
+    path('', views.dashboard, name='dashboard'),
+
+    #PVSystem Pages
+    path('pvsystems/add/', PVSystemCreateView.as_view(), name='pvsystem_create'),
+    path('pvsystems/<uuid:pk>/', PVSystemDetailView.as_view(), name='pvsystem_detail'),
+    path('pvsystems/<uuid:pk>/', PVSystemListView.as_view(), name='pvsystems_list'),
+    path('pvsystems/<uuid:pk>/edit/', PVSystemUpdateView.as_view(), name='pvsystem_edit'),
+    path('pvsystems/<uuid:pk>/delete/', PVSystemDeleteView.as_view(), name='pvsystem_delete'),
+
     
 ]
--- afl_webserver/main/views.py
+++ afl_webserver/main/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
+from django.urls.base import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,  
+    UpdateView,
+    DeleteView,
+)
+from . models import PVSystem
 
-# Create your views here.
+"""
+Landing Page
+"""
+def dashboard(request):
+    context ={
+        "title": "Dashboard",
+        "num_of_pv_systems": PVSystem.objects.count(),
+    }
+    return render(request, 'main/index.html', context)
+
+"""
+Create Views
+"""
+class PVSystemCreateView(CreateView):
+    #form_class = PVSystemForm
+    model = PVSystem
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add PV System'
+        return context
+
+"""
+Detail Views
+"""
+class PVSystemDetailView(DetailView):
+    model = PVSystem
+    template_name = 'main/pvsystem_list.html'
+
+    def get_context_data(self, **kwargs):
+        pv_system = PVSystem.objects.get(pk=self.kwargs['pk'])
+        context = super().get_context_data(**kwargs)
+        context['title'] = pv_system.name
+        return context
+
+    
+"""
+List Views
+"""
+class PVSystemListView(ListView):
+    model = PVSystem
+    paginate_by = 50
+    template_name = 'main/pvsystem_list.html'
+
+
+"""
+Update Views
+"""
+class PVSystemUpdateView(UpdateView):
+    #form_class = PVSystemForm
+    model = PVSystem
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pv_system = PVSystem.objects.get(pk=self.kwargs['pk'])
+        context['title'] = f'Update {pv_system.name} details'
+        return context
+
+"""
+Delete Views
+"""
+class PVSystemDeleteView(DeleteView):
+    model = PVSystem
+    success_url = reverse_lazy('pvsystems_list')
